fa982d0fe7f801a832dcda37dbc96d8dcd5ca0f2
Andy Holt <andy.m.holt@gmail.com>
2019-04-08 21:32:43 +0100

    Red: Created test "Checkout_WhenNothingIsScanned_TheTotalPriceIsZero". The test currently fails as the "Checkout" class doesn't exist yet.

---
+++ Checkout.UnitTests/CheckoutTests.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Checkout.UnitTests
+{
+    [TestClass]
+    public class CheckoutTests
+    {
+        private Checkout checkout = new Checkout();
+
+        [TestMethod]
+        public void Checkout_WhenNothingIsScanned_TheTotalPriceIsZero()
+        {
+            Assert.AreEqual(0, this.checkout.GetTotalPrice());
+        }
+    }
+}
